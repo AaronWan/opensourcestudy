da55e9de07a18efaf5e422e8ceb6ee52b8439cd4
AaronWan <sejin@126.com>
2022-10-25 10:20:28 +0800

    mvel expression template

--- MVEL/src/main/java/mvel/MvelTest.java
+++ MVEL/src/main/java/mvel/MvelTest.java
@@ -1,10 +1,16 @@
 package mvel;
 
+import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.mvel2.templates.TemplateRuntime;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * https://cloud.tencent.com/developer/article/2048242
+ */
 public class MvelTest {
     public static void main(String[] args) {
         String template = "Hello, my name is @{name.toUpperCase()}";
@@ -16,11 +22,26 @@ public class MvelTest {
 
         template = "@{a+b}";
         vars = new HashMap();
-        vars.put("a",1);
-        vars.put("b",2);
+        vars.put("a", 1);
+        vars.put("b", 2);
 
         Integer rst = (Integer) TemplateRuntime.eval(template, vars);
         System.out.println(rst);
 
+
+        template = "Hello, my name is @{k.name}";
+        Map person= Maps.newHashMap();
+        person.put("name","abc");
+        vars = new HashMap();
+        vars.put("k", person);
+
+        output = (String) TemplateRuntime.eval(template, vars);
+        System.out.println(output);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class Person {
+        String name;
     }
 }
