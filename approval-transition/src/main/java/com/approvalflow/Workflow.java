7a7821a3b6fa0eec22020972bc1ae0dc471db5a3
AaronWan <sejin@126.com>
2023-08-24 00:17:29 +0800

    update

--- approval-transition/src/main/java/com/approvalflow/Workflow.java
+++ approval-transition/src/main/java/com/approvalflow/Workflow.java
@@ -3,6 +3,7 @@ package com.approvalflow;
 import com.google.common.collect.Lists;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
 import lombok.Data;
 import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
@@ -11,6 +12,7 @@ import org.apache.commons.lang.StringUtils;
 import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -23,25 +25,30 @@ public class Workflow {
     String name;
     private List<Node> activities;
     private List<Transition> transitions;
-    private Map<String,Node> activityMap;
-    private Map<String,Transition> transitionMap;
+    private Map<String, Node> activityMap;
+    private Map<String, Transition> transitionMap;
 
     public void init() {
-        activityMap=activities.stream().collect(Collectors.toMap(node->node.getId(),node->node));
-        transitionMap=transitions.stream().peek(item->{
+        activityMap = activities.stream().collect(Collectors.toMap(node -> node.getId(), node -> node));
+        transitionMap = transitions.stream().peek(item -> {
             item.setFrom(activityMap.get(item.getFromId()));
             item.setTo(activityMap.get(item.getToId()));
-        }).collect(Collectors.toMap(transition->transition.getId(),transition->transition));
+        }).collect(Collectors.toMap(transition -> transition.getId(), transition -> transition));
     }
 
-    public List<Transition> findCurrentActivityIdTo(String fromId){
-        List<Transition> result= Lists.newArrayList();
+    public List<Transition> findCurrentActivityIdTo(String fromId) {
+        List<Transition> result = Lists.newArrayList();
         for (Transition transition : transitions) {
-            if(transition.getFromId().equals(fromId)){
-                Node node=transition.to;
-                if(StringUtils.isBlank(node.getName())){
+            if (transition.getFromId().equals(fromId)) {
+                Node node = transition.to;
+                String action = getAction(transition);
+                if("go_back".equals(action)||"reject".equals(action)){
+                    continue;
+                }
+                System.out.println(fromId + " " + node.getId()+" "+action);
+                if (StringUtils.isBlank(node.getName())) {
                     result.addAll(findCurrentActivityIdTo(transition.toId));
-                }else{
+                } else {
                     result.add(transition);
                 }
             }
@@ -49,14 +56,25 @@ public class Workflow {
         return result;
     }
 
-    public List<Transition> findCurrentActivityIdFrom(String id){
-        List<Transition> result= Lists.newArrayList();
+    private String getAction(Transition transition) {
+        if (Objects.isNull(transition.condition)) {
+            return "";
+        }
+        try {
+            return (String) ((LinkedTreeMap) (((LinkedTreeMap.Entry) ((transition.condition).entrySet().toArray()[2])).getValue())).values().stream().findFirst().orElse(null);
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public List<Transition> findCurrentActivityIdFrom(String id) {
+        List<Transition> result = Lists.newArrayList();
         for (Transition transition : transitions) {
-            if(transition.getToId().equals(id)){
-                Node node=transition.from;
-                if(StringUtils.isBlank(node.getName())){
+            if (transition.getToId().equals(id)) {
+                Node node = transition.from;
+                if (StringUtils.isBlank(node.getName())) {
                     result.addAll(findCurrentActivityIdFrom(transition.fromId));
-                }else{
+                } else {
                     result.add(transition);
                 }
             }
@@ -66,11 +84,11 @@ public class Workflow {
 
     @SneakyThrows
     public static void main(String[] args) {
-        FileInputStream fis=new FileInputStream(Workflow.class.getResource("/definition.json").getPath());
+        FileInputStream fis = new FileInputStream(Workflow.class.getResource("/definition.json").getPath());
         String content = IOUtils.toString(fis);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Workflow workflow=gson.fromJson(content, Workflow.class);
+        Workflow workflow = gson.fromJson(content, Workflow.class);
         workflow.init();
-        System.out.println(gson.toJson(workflow.findCurrentActivityIdTo("1682669396145")));
+        gson.toJson(workflow.findCurrentActivityIdTo("1658104272890"));
     }
 }
